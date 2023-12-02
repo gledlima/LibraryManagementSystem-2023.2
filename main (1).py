11f74664d712de7e38581621d076480cb3f1fe9c
Gledson Lima <58941795+gledlima@users.noreply.github.com>
2023-12-02 08:22:12 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,20 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def menu():
+    print()
+    print("-------- Menu ----------")
+    print('Pressione A para pesquisar algum Livro')
+    print('Pressione B para acessar a seção de xxxxxxx')
+    print('Pressione C para acessar a seção de xxxxxxxx')
+    print('Pressione X para sair')
+    print()
+    
+    action = input('O que você deseja fazer? ')
+    action = action.lower()
+    action = action[0]
+    
+    if action == 'a':
+        time
+    
+    
